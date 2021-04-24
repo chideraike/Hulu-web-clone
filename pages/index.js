8870115127ff748c6f8d4cbef92b03a8d80d412b
chideraike <chideraike.s@gmail.com>
2021-04-24 23:24:30 +0000

    :sparkles: built header items

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Hulu 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Let's build Hulu 2.0</h1>
-      <h1>Let's build Hulu 2.0</h1>
+      {/* Header */}
+      <Header />
+
+      {/* Nav */}
+
+      {/* Results */}
     </div>
   )
 }
