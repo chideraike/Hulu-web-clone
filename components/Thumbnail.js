c09456a2a6f05c0315675bd1712a361a1458d72d
chideraike <chideraike.s@gmail.com>
2021-04-26 00:44:31 +0000

    :iphone: results displayed are responsive

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -15,15 +15,14 @@ function Thumbnail({ result }) {
                 height={1080}
                 width={1920}
             />
-            <div>
-                <p className="truncate">{result.overview}</p>
+            <div className="p-2">
+                <p className="truncate max-w-md">{result.overview}</p>
 
                 <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
                     {result.title || result.original_name}
                 </h2>
 
-                <p className="flex items-center opacity-0 group-hover:opacity-100
-                ">
+                <p className="flex items-center opacity-0 group-hover:opacity-100">
                     {result.media_type && `${result.media_type} •`}{" "}
                     {result.release_date || result.first_air_date} •{" "}
                     <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
